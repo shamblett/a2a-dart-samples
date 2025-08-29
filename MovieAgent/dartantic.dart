c1123a102a38b6172bfab5ea5ae30cdc12aab552
Steve Hamblett <steve.hamblett@linux.com>
2025-08-29 10:47:05 +0100

    Initial setup

---
+++ MovieAgent/dartantic.dart
@@ -0,0 +1,11 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 29/08/2025
+* Copyright :  S.Hamblett
+*/
+
+import 'package:dartantic_ai/dartantic_ai.dart';
+
+
+
