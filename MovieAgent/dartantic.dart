c5c6ca544236c0c7ca2524c4344c752f594b8421
Steve Hamblett <steve.hamblett@linux.com>
2025-08-29 11:27:27 +0100

    Dartantic

--- MovieAgent/dartantic.dart
+++ MovieAgent/dartantic.dart
@@ -5,11 +5,33 @@
 * Copyright :  S.Hamblett
 */
 
+import 'dart:async';
+
 import 'package:dartantic_ai/dartantic_ai.dart';
+import 'package:dartantic_interface/dartantic_interface.dart';
+import 'package:json_schema/json_schema.dart';
 
 import 'config.dart';
 
+// Json schemas
+final mustBeStringSchema = {'type': 'string'};
+final searchMoviesSchema = JsonSchema.create(mustBeStringSchema);
+final searchPeopleSchema = JsonSchema.create(mustBeStringSchema);
+
+// Tools
+final searchMoviesOnCall = ((args) async => {});
+
+final searchMovies = Tool(name: 'searchMovies', description: 'Search TMDB for movies by title',
+inputSchema: searchMoviesSchema,onCall: searchMoviesOnCall);
+
+final searchPeopleOnCall = ((args) async => {});
+
+final searchPeople = Tool(name: 'searchPeople', description: 'Search TMDB for or people by name',
+    inputSchema: searchPeopleSchema,onCall: searchPeopleOnCall);
+
+
+// Provider
 final geminiProvider = GoogleProvider(apiKey: googleApIKey);
 
-final chatModel = geminiProvider.createChatModel(name: 'gemini-2.0-flash', tools: []);
+final chatModel = geminiProvider.createChatModel(name: 'gemini-2.0-flash', tools: [searchMovies]);
 
