a5292681803d5c61c89423e6249a1dc777cc3b7a
Steve Hamblett <steve.hamblett@linux.com>
2025-08-29 10:51:43 +0100

    Dartantic

--- MovieAgent/dartantic.dart
+++ MovieAgent/dartantic.dart
@@ -7,5 +7,9 @@
 
 import 'package:dartantic_ai/dartantic_ai.dart';
 
+import 'config.dart';
 
+final geminiProvider = GoogleProvider(apiKey: googleApIKey);
+
+final chatModel = geminiProvider.createChatModel(name: 'gemini-2.0-flash', tools: []);
 
