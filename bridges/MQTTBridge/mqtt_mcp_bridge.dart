bc0bcbf339515e812b8dbebdffd23717e68a57a6
Steve Hamblett <steve.hamblett@linux.com>
2025-09-21 10:44:42 +0100

    MQTT bridge

--- bridges/MQTTBridge/mqtt_mcp_bridge.dart
+++ bridges/MQTTBridge/mqtt_mcp_bridge.dart
@@ -6,7 +6,40 @@
 */
 
 import 'package:a2a/a2a.dart';
+import 'package:colorize/colorize.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+
+/// MQTT Bridge
+///
+class MqttMcpBridge extends A2AMCPBridge {
+  // Broker URL
+  String _serverUrl = '';
+
+  // Client Identifier
+  String _clientIdentifier = '';
+
+  late final MqttServerClient _client;
+
+  // Subscribed topics
+  List<String> _subscribedTopics = [];
+
+  // Received messages
+  List<MqttPublishMessage> _receivedMessages = [];
+
+  MqttMcpBridge() : super() {}
+}
 
 // Main server
-void main() {
+void main() async {
+  // Create and start the bridge
+  print('${Colorize('Creating the MQTT MCP Bridge').blue()}');
+  MqttMcpBridge mqttMcpBridge = MqttMcpBridge();
+  try {
+    await mqttMcpBridge
+        .startServer(); // Set your port if you do not want the default
+  } catch (e) {
+    print('${Colorize('MQTT MCP Bridge failed to start $e').red()}');
+    return;
+  }
 }
