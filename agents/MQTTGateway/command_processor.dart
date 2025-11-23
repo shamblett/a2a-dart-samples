d7b00bee7107b4a3f696753184e2fcd4aa931850
Steve Hamblett <steve.hamblett@linux.com>
2025-11-23 10:40:48 +0000

    Command processor

---
+++ agents/MQTTGateway/command_processor.dart
@@ -0,0 +1,11 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 07/11/2025
+* Copyright :  S.Hamblett
+*/
+
+///
+/// Gateway command processor.
+///
+class CommandProcessor {}
