913f6c3ada0c576664f9abb9da25a6b01726ad02
Steve Hamblett <steve.hamblett@linux.com>
2025-11-28 09:56:17 +0000

    Command tests

--- agents/MQTTGateway/test/command_test.dart
+++ agents/MQTTGateway/test/command_test.dart
@@ -11,6 +11,7 @@ library;
 import 'package:test/test.dart';
 
 import '../src/command.dart';
+import '../src/mqtt_manager.dart';
 
 void main() {
   test('Empty command', () async {
@@ -26,7 +27,38 @@ void main() {
     expect(cmd, isNull);
   });
   test('Connect - No Broker URL', () async {
-    final cmd = Command.fromJson('{ "billy" : "fred" }');
-    expect(cmd, isNull);
+    final cmd = Command.fromJson(
+      '{ "command" : "connect", "client_id" : "www" }',
+    );
+    expect(cmd, isNotNull);
+    expect(cmd!.isValid, isFalse);
+  });
+  test('Connect - Defaults', () async {
+    final cmd = Command.fromJson(
+      '{ "command" : "connect", "broker_url" : "localhost"}',
+    );
+    expect(cmd, isNotNull);
+    expect(cmd!.isValid, isTrue);
+    expect(cmd is Connect, isTrue);
+    final connect = cmd as Connect;
+    expect(connect.brokerUrl, 'localhost');
+    expect(connect.port, 1883);
+    expect(connect.clientId, MqttManager.defaultClientId);
+    expect(connect.username, isNull);
+    expect(connect.password, isNull);
+  });
+  test('Connect - All parameters', () async {
+    final cmd = Command.fromJson(
+      '{ "command" : "connect", "broker_url" : "localhost", "port" : 2000, "client_id" : "www", "user_name" : "billy", "password" : "dd"}',
+    );
+    expect(cmd, isNotNull);
+    expect(cmd!.isValid, isTrue);
+    expect(cmd is Connect, isTrue);
+    final connect = cmd as Connect;
+    expect(connect.brokerUrl, 'localhost');
+    expect(connect.port, 2000);
+    expect(connect.clientId, 'www');
+    expect(connect.username, 'billy');
+    expect(connect.password, 'dd');
   });
 }
