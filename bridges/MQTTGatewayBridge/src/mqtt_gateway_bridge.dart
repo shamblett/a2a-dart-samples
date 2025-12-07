bbf67fb71d802aff9ecff426d145906f16730fcf
Steve Hamblett <steve.hamblett@linux.com>
2025-12-07 10:46:39 +0000

    Status tool

--- bridges/MQTTGatewayBridge/src/mqtt_gateway_bridge.dart
+++ bridges/MQTTGatewayBridge/src/mqtt_gateway_bridge.dart
@@ -6,13 +6,57 @@
 */
 
 import 'package:a2a/a2a.dart';
+import 'package:mcp_dart/mcp_dart.dart';
 
 import 'log.dart';
 
 /// MQTT Bridge
 ///
 class MqttGatewayBridge extends A2AMCPBridge {
-  MqttGatewayBridge() : super();
+  MqttGatewayBridge() : super() {
+    // Initialise the MQTT gateway tools
+    _initialiseTools();
+  }
+
+  // Status callback
+  Future<CallToolResult> _statusCallback({
+    Map<String, dynamic>? args,
+    RequestHandlerExtra? extra,
+  }) async {
+    if (args == null) {
+      Log.warn('_registerAgentCallback - args are null');
+      return CallToolResult.fromContent(
+        content: [TextContent(text: '_registerAgentCallback - args are null')],
+        isError: true,
+      );
+    }
+  }
+
+  // Initialise the MQTT Gateway tools
+  void _initialiseTools() {
+    // Status
+    // Get the status of the MQTT Gateway
+    var inputSchema = ToolInputSchema(
+      properties: {
+        "url": {"type": "string", "description": "The agent URL"},
+      },
+      required: ["url"],
+    );
+    var outputSchema = ToolOutputSchema(
+      properties: {
+        "agent_name": {"type": "string", "description": "Name of the agent"},
+        "url": {"type": "string", "description": "Url of the agent"},
+      },
+      required: ["agent_name", "url"],
+    );
+    final registerAgent = Tool(
+      name: 'register_agent',
+      description: 'A2A Bridge Register Agent',
+      inputSchema: inputSchema,
+      outputSchema: outputSchema,
+    );
+    registerTool(registerAgent, _statusCallback);
+  }
 }
 
 // Main server
@@ -21,8 +65,9 @@ void main() async {
   Log.info('Creating the MQTT Gateway Bridge');
   MqttGatewayBridge mqttMcpBridge = MqttGatewayBridge();
   try {
-    await mqttMcpBridge
-        .startServer(port: 10005); // Set your port if you do not want the default
+    await mqttMcpBridge.startServer(
+      port: 10005,
+    ); // Set your port if you do not want the default
   } catch (e) {
     Log.fatal('MQTT Gateway Bridge failed to start $e');
     return;
